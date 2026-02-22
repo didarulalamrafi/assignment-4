7c78372fe3ebbb9a833bf7fc3eee78c968ca3b4a
Didarul Alam <didarulalamrafi1312@gmail.com>
2026-02-22 19:49:36 +0600

    fixd

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const titleInterviewBtn = document.getElementById("interview-main-btn");
+titleInterviewBtn.addEventListener("click", function () {
+  //   console.log("interveiw click");
+  alert("click");
+});
 
 // let count = 0;
 // let total = 8;
@@ -5,83 +10,60 @@
 // select.forEach((element)=> {
 //     // console.log(element);
 //     element.addEventListener("click", function(){
-//         if (total < 0) 
+//         if (total < 0)
 //             {
 //                 return alert("Invaild Interview");
-               
+
 //             };
 //         document.getElementById('total').innerText = total;
 //         document.getElementById('interview-number').innerText = count;
 //         count ++;
-//         total = total - 1;  
+//         total = total - 1;
 //     });
 // });
 
+// const jobs = 8;
 
-const jobs = 8
+// function updown(click, job = 0, callBack = () => {}) {
+//   let count = 0;
+//   let total = job;
 
+//   const select = document.querySelectorAll(click);
 
-function updown( click ,    job = 0 , callBack = ()=> {}){
-let count = 0;
-let total = job;
+//   select.forEach((element) => {
+//     element.addEventListener("click", function () {
+//       if (total <= 0) return alert("Invaild Interview");
 
-const select = document.querySelectorAll(click)
+//       count++;
+//       total = total - 1;
+//       callBack({ interview: count, total });
+//     });
+//   });
+// }
 
-select.forEach((element)=> {
-    element.addEventListener("click", function(){
-        if (total <= 0) return alert("Invaild Interview");      
-        
-        count ++;
-        total = total - 1;
-        callBack({interview :count , total }) 
-    });
-});
+// const rejectedFun = ({ interview = 0, total = 0 }) => {
+//   const interviews = document.getElementById("interview");
+//   document.getElementById("total").innerText = total;
 
-}
+//   const jo = document.getElementById("job");
 
+//   interviews.addEventListener("click", () => {
+//     const isActive = interviews.classList.toggle("active");
 
+//     if (isActive) {
+//       jo.classList.add("block");
+//       jo.classList.remove("hidden");
+//     } else {
+//       jo.classList.add("hidden");
+//       jo.classList.remove("block");
+//     }
+//   });
+// };
 
+// rejectedFun({ total: jobs, interview: 0 });
 
-
-const rejectedFun = ({interview = 0  , total = 0}) => {
-        const interviews = document.getElementById('interview')
-        document.getElementById('total').innerText = total;
-      
-       const jo = document.getElementById('job')
-
-
-         interviews.addEventListener("click",() => {
-             const isActive = interviews.classList.toggle("active");
-
-        
-        if (isActive) {
-            
-            jo.classList.add("block")
-            jo.classList.remove("hidden")
-        } else {
-            
-            jo.classList.add("hidden")
-            jo.classList.remove("block")
-        }
-        
-            
-        })
-       
-}
-
-rejectedFun({ total : jobs , interview : 0})
-
-
- updown(".interview-btn" , jobs , (({interview  , total}) => {
-  
-    rejectedFun({interview  , total})
-    document.getElementById('total').innerText = total;
-     document.getElementById('interview-number').innerText = interview;
-
- }));
- 
-
-
-
-
-
+// updown(".interview-btn", jobs, ({ interview, total }) => {
+//   rejectedFun({ interview, total });
+//   document.getElementById("total").innerText = total;
+//   document.getElementById("interview-number").innerText = interview;
+// });
